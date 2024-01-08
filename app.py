dbd985a1c1644b212e54569a6a7b78b655a6ad31
Jocelyn Lee <jocelynlee@Jocelyns-MacBook-Air.local>
2024-01-08 09:08:42 -0800

    added celery support

--- app.py
+++ app.py
@@ -1,32 +1,57 @@
 # app.py
-import secrets
-from main import WishList
-from datetime import datetime
-from functools import wraps
-
-from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Length, EqualTo
+from flask import Flask
+from celery import Celery, Task
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_caching import Cache
+from main import WishList
+import secrets
+from functools import wraps
+from flask import render_template, Blueprint, redirect, url_for, flash, request, jsonify
+from flask_login import login_user, login_required, logout_user, current_user
+# from models.models import *
+# from models.config import db
+# from tasks import create_book
+from flask_sqlalchemy import SQLAlchemy
 
-from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
-from flask_caching import Cache  # Use a caching library like Flask-Caching
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wishlist.db'
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wishlist.db'
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+    app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+    app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+    return app
+
+kobo_wishlist = WishList()
+app = create_app()
+celery_app = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery_app.conf.update(app.config)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-kobo_wishlist = WishList()
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+# route_bp = Blueprint("routes", __name__, template_folder="templates")
+
+@celery_app.task
+def create_book(current_user_id, book_id):
+    this_book = kobo_wishlist.add(book_id)
+    if not this_book:
+        return 'failed to add this book!'
+    
+    return this_book
+
+from datetime import datetime
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+# from app import db
+
 
 # Database model for User
 class User(db.Model, UserMixin):
@@ -63,19 +88,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+# route_bp = Blueprint("routes", __name__, template_folder="templates")
+
 def generate_api_key():
     return secrets.token_urlsafe(16)
 
-def create_book(this_user, book_id):
-    this_book = kobo_wishlist.add(book_id)
-    if not this_book:
-        return 'failed to add this book!'
+def update_db(current_user_id, this_book):
     new_book = Book(
         title=this_book["title"], 
         book_id=this_book["id"], 
         price=this_book["price"], 
         timestamp=this_book["timestamp"],
-        user=this_user)
+        user_id=current_user_id)
     
     try:
         db.session.add(new_book)
@@ -84,6 +108,7 @@ def create_book(this_user, book_id):
         return redirect(url_for('wishlist'))
     except Exception as e:
         db.session.rollback()
+        # flash(str(e))
         flash('Book with this ID already exists in the wishlist.', 'danger')
         # return redirect(url_for('index'))
         return redirect(url_for('wishlist'))
@@ -92,12 +117,11 @@ def create_book(this_user, book_id):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
+# @celery_app.route('/')
+@app.route('/', methods=['GET'])
 @login_required
 def index():
     books = current_user.books
-    # books = Book.query.all()
-    # return redirect(url_for('wishlist'))
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -132,13 +156,32 @@ def login():
 @login_required
 def add_book():    
     return login_required(add_book_login)()
-
+        
 @login_required
 def add_book_login():
     if request.method == 'POST':
         # Your route logic here
         book_id = request.form['book_id']
-        return create_book(current_user, book_id)
+        # Call the Celery task asynchronously using delay()
+        new_book_object = create_book.apply_async(args=[current_user.id, book_id], countdown=1)
+        new_book_dict = new_book_object.get()
+        new_book = Book(
+            title=new_book_dict["title"], 
+            book_id=new_book_dict["id"], 
+            price=new_book_dict["price"], 
+            timestamp=new_book_dict["timestamp"],
+            user_id=current_user.id)
+        
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+            flash('Book added to wishlist successfully!', 'success')
+            return redirect(url_for('wishlist'))
+        except Exception as e:
+            db.session.rollback()
+            # flash(str(e))
+            flash('Book with this ID already exists in the wishlist.', 'danger')
+            return redirect(url_for('wishlist'))
     else:
         return 'Invalid request method'
 
@@ -209,12 +252,12 @@ def add_book_api_key():
     user = request.user  # Access the user from the request context
     form_data = request.values
     return create_book(user, form_data["book_id"])
-    # Now you can use the user object to associate the book with the user
-    # book_data = request.get_json()
-    # new_book = Book(title=book_data['title'], user_id=user.id)
-    # return jsonify({'message': 'Book added successfully'})
 
+
+    
 if __name__ == '__main__':
+    # app.run(debug=True)
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
